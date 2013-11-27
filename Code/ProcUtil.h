ff05b2f60886095067d26f033c318ed3d8229789
Daniel Noyes <reiuiji@gmail.com>
2013-11-27 11:51:46 -0500

    Implement a few features for the utility to grab the process data.

--- Code/ProcUtil.h
+++ Code/ProcUtil.h
@@ -1,12 +1,12 @@
 
 //process sturcture, will hold all the information for the process
-typedef struct Process  
+typedef struct Process
 {
 	int P_ID;
 	int CPU_BURST;
 	int IO_BURST;
 	int PRIORITY;
-	
+
 } PROCESS;
 
 
@@ -15,15 +15,17 @@ typedef struct ProcNode
 {
 	struct Process *PROCESS;
 	struct ProcNode *NEXT;
+	struct ProcNode *PREV;
 } PROCNODE;
 
 //stucture for the process queue that will hold all the process
 typedef struct Queue
 {
 	int NUM_P;  // how many processes are in the queue
-	ProcNode *TOP;  //points to top of the list 
-	ProcNode *BOT;  //points to the bottom of the list
+	PROCNODE *TOP;  //points to top of the list
+	PROCNODE *BOT;  //points to the bottom of the list
 } QUEUE;
 
-	   
+
+
 

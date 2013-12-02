23ec8b7956491b0576e67c14277e07e6701daed6
kkevelson <kkevelson@umassd.edu>
2013-12-01 20:12:35 -0500

    Update FCFS.c
    I'm adding text now to explain my approach to this project's resolution. Unfortunately, another resolution is pesky; I can't see the screen clearly.

---
+++ Code/FCFS.c
@@ -0,0 +1,5 @@
+//Get arrival and service times for each process from queue.
+
+//Get total time for each process and store in an array
+//Then sort the processes based on arrival time. Use service time to set time intervals.
+

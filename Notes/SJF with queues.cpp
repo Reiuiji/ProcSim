44179e07439eddfce98bee56f62ff50184fafc9c
BD <bdoiron@umassd.edu>
2013-12-04 19:50:11 -0500

    SJF with Queues
    Updated SJF with Queues

--- Notes/SJF with queues.cpp
+++ Notes/SJF with queues.cpp
@@ -33,7 +33,7 @@ int main()
 	int DEVICE_CPU_BURST, DEVICE_IO_BURST;
 	int READY_CPU_WITH_IO, READY_CPU_BURST, READY_IO_BURST;
 	int LOP = 0;
-	int READY_QUEUE[50], IO_QUEUE[50]; // The queues. These are here because I'm bad at things.
+	int READY_QUEUE[50] = {-1}, IO_QUEUE[50] = {-1}; // The queues. These are here because I'm bad at things.
 
 	// inputs counters for each of the processes in the queues so it knows when to end.
 	int READY_QUEUE_AMT = 0, CPU_QUEUE_amount = 0, IO_QUEUE_AMT = 0;
@@ -50,9 +50,9 @@ int main()
 		// This makes a note of whether or not the process will have to stop halfway in due to the IO burst.
 		if(Q_Pos[i].IO_BURST > 0)
 		{Q_Pos[i].CPU_WITH_IO =	(CPU_BURST_VAR[i]/2);}
-		// If it doesn't, it sets the half burst time to 0.
-		else 
-		{Q_Pos[i].CPU_WITH_IO = 0;}
+		// If it doesn't, it sets the half burst time to -1.
+		else
+		{Q_Pos[i].CPU_WITH_IO = -1;}
 	}
 
 	// Before the processes are loaded into the Ready Queue, they're sorted.
@@ -66,12 +66,12 @@ int main()
 /*====================================================================================================*/
 /*====================================================================================================*/
 
-	while(LOP!=10)
+	while(LOP<10)
 	{
 		if(CPU_FLAG == 1)
 		{
 			// Loads all of the values from the process in the CPU.
-			for(i=0; i<READY_QUEUE_AMT; i++)
+			for(i=0; i<NUM_PROCESSES; i++)
 			{
 				if(CPU_QUEUE == Q_Pos[i].P_ID)
 				{
@@ -86,23 +86,23 @@ int main()
 			{
 				IO_QUEUE[IO_QUEUE_AMT] = CPU_QUEUE;
 				IO_QUEUE_AMT++;
-				CPU_QUEUE = NULL;
+				CPU_QUEUE = -1;
 				CPU_FLAG = 0;
 			}
 
 			// Checks the process data. If there's a program that has finished its CPU burst and has finished its IO burst, it gets removed.
 			else if((LOADED_CPU_BURST == 0) && (LOADED_IO_BURST == 0))
 			{
-				CPU_QUEUE = NULL;
+				CPU_QUEUE = -1;
 				CPU_FLAG = 0;
 			}
 
-			// Checks the process data. If there's a program that has finished half of its CPU burst but needs to finish its IO, it moves it to the device queue. 
-			else if((LOADED_CPU_WITH_IO == 0) && (LOADED_IO_BURST > 0))
+			// Checks the process data. If there's a program that has finished half of its CPU burst but needs to finish its IO, it moves it to the device queue.
+			else if((LOADED_CPU_WITH_IO <= 0) && (LOADED_IO_BURST > 0))
 			{
 				IO_QUEUE[IO_QUEUE_AMT] = CPU_QUEUE;
 				IO_QUEUE_AMT++;
-				CPU_QUEUE = NULL;
+				CPU_QUEUE = -1;
 				CPU_FLAG = 0;
 			}
 
@@ -112,17 +112,33 @@ int main()
 		// Checks if there's anything in the IO queue
 		if(IO_QUEUE_AMT >= 1)
 		{
-			for(i=0; i=IO_QUEUE_AMT; i++)
-			{
-				for(j=0; j=NUM_PROCESSES; j++)
+            for(i=0; i<IO_QUEUE_AMT;i++)
+            {
+                if(IO_QUEUE_AMT == 0)
+                {
+                    for(j=0; j<NUM_PROCESSES; j++)
+                    {
+                        // Goes through a cycle to grab the data for each of the Processes in the IO Queue
+                        if(IO_QUEUE == Q_Pos[j].P_ID)
+                        {
+                            DEVICE_CPU_BURST = Q_Pos[j].CPU_BURST;
+                            DEVICE_IO_BURST = Q_Pos[j].IO_BURST;
+                        }
+                    }
+                }
+				else
 				{
-					// Goes through a cycle to grab the data for each of the Processes in the IO Queue
-					if(IO_QUEUE[i] == Q_Pos[j].P_ID)
+					for(j=0; j<NUM_PROCESSES; j++)
 					{
-						DEVICE_CPU_BURST = Q_Pos[j].CPU_BURST;
-						DEVICE_IO_BURST = Q_Pos[j].IO_BURST;
+						// Goes through a cycle to grab the data for each of the Processes in the IO Queue
+						if(IO_QUEUE[i] == Q_Pos[j].P_ID)
+						{
+							DEVICE_CPU_BURST = Q_Pos[j].CPU_BURST;
+							DEVICE_IO_BURST = Q_Pos[j].IO_BURST;
+						}
 					}
 				}
+
 				// If there's a process in the IO queue that doesn't have any burst but has a CPU burst left
 				if((DEVICE_IO_BURST == 0) && (DEVICE_CPU_BURST >=1))
 				{
@@ -134,11 +150,11 @@ int main()
 					for(j=i; j<IO_QUEUE_AMT; j++)
 					{
 						IO_QUEUE[j] = IO_QUEUE[j+1];
-						IO_QUEUE[j+1] = NULL;
+						IO_QUEUE[j+1] = -1;
 					}
 					IO_QUEUE_AMT--;
 					// and it decrements so it doesn't skip any elements.
-					i--;
+					//i--;
 				}
 
 				// if there is a process that has finished and doesn't have a CPU burst
@@ -148,12 +164,12 @@ int main()
 					for(j=i; j<IO_QUEUE_AMT; j++)
 					{
 						IO_QUEUE[j] = IO_QUEUE[j+1];
-						IO_QUEUE[j+1] = NULL;
+						IO_QUEUE[j+1] = -1;
 					}
 					IO_QUEUE_AMT--;
 
 					// and it decrements so it doesn't skip any elements.
-					i--;
+					//i--;
 				}
 			}
 		}
@@ -179,52 +195,78 @@ int main()
 				for(i=0; i<READY_QUEUE_AMT; i++)
 				{
 					READY_QUEUE[i] = READY_QUEUE[i+1];
-					READY_QUEUE[i+1] = NULL;
+					READY_QUEUE[i+1] = -1;
 				}
 				READY_QUEUE_AMT--;
 			}
 			else if((READY_CPU_WITH_IO >= 1) || (READY_CPU_BURST >= 1))
 			{
-				READY_QUEUE[0] = CPU_QUEUE;
+				CPU_QUEUE = READY_QUEUE[0];
 				CPU_FLAG = 1;
 				for(i=0; i<READY_QUEUE_AMT; i++)
 				{
 					READY_QUEUE[i] = READY_QUEUE[i+1];
-					READY_QUEUE[i+1] = NULL;
+					READY_QUEUE[i+1] = -1;
 				}
 				READY_QUEUE_AMT--;
 			}
 		}
 
-		if(READY_QUEUE[0] != NULL)
+		if(READY_QUEUE[0] != -1)
 		{
 			printf("Ready Queue: %d", READY_QUEUE[0]);
-			for(i=1; i>NUM_PROCESSES; i++)
+			for(i=1; i>READY_QUEUE_AMT; i++)
 			{
-				while(READY_QUEUE[i] != NULL)
-				{
 					printf("-%d", READY_QUEUE[i]);
-				}
 			}
 			printf("\n");
 		}
-		if(IO_QUEUE[0] != NULL)
+		if(IO_QUEUE[0] != -1)
 		{
 			printf("Device Queue: %d", IO_QUEUE[0]);
-			for(i=1; i>NUM_PROCESSES; i++)
+			for(i=1; i>IO_QUEUE_AMT; i++)
 			{
-				while(IO_QUEUE[i] != NULL)
-				{
 					printf("-%d",IO_QUEUE[i]);
-				}
 			}
 			printf("\n");
 		}
-		if(CPU_QUEUE != NULL)
+		if(CPU_QUEUE != -1)
 		{
 			printf("CPU Queue: %d\n\n", CPU_QUEUE);
 		}
+
+/* ------------------------------------------- */
+
+		if(IO_QUEUE[0] != -1)
+		{
+			for(i=0; i<NUM_PROCESSES; i++)
+			{
+				for(j=0; j<IO_QUEUE_AMT; j++)
+				{
+					if(IO_QUEUE[i] == Q_Pos[j].P_ID)
+					{
+						Q_Pos[j].IO_BURST--;
+					}
+				}
+			}
+		}
+		if(CPU_FLAG = 1)
+		{
+			for(i=0; i<NUM_PROCESSES; i++)
+			{
+				if(CPU_QUEUE == Q_Pos[i].P_ID)
+				{
+					if(Q_Pos[i].CPU_WITH_IO >= 1)
+					{
+						Q_Pos[i].CPU_WITH_IO--;
+					}
+					Q_Pos[i].CPU_BURST--;
+				}
+			}
+		}
+
 	LOP++;
+
 	}
 	return 0;
 }

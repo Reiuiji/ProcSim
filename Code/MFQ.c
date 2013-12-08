5b4dd2d49e26643d3fabba38c188ff15e23b6410
kkevelson <kkevelson@umassd.edu>
2013-12-08 18:26:41 -0500

    Update MFQ.c

--- Code/MFQ.c
+++ Code/MFQ.c
@@ -8,8 +8,21 @@
 struct queue1;
 struct queue2;
 struct queue3;
+int quantum;
+
 void MFQ(){
 
-// Implementation of MFQ
+/*Getting burst times from ReadyQueue
+Get divide into three queues as a result
+Place processes with burst time>3 on queue 1
+Place processes with burst times between 3 and 8 on queue 2
+Place processes with burst times greater than 8 on queue3
+Use RR to sort queue 1 and 2. 
+Use FCFS to sort queue3
+Place sorted queues on IO queue(concatenate) in order */
+
+
+
+
 
 }
